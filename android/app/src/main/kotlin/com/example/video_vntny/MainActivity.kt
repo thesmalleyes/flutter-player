a227232233f5121692e67696189b68d6a1e151fe
Bayu Ramadeza <bayuramadeza@gmail.com>
2023-04-29 05:23:25 +0700

    init project

---
+++ android/app/src/main/kotlin/com/example/video_vntny/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.video_vntny
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:video_vntny/home/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Video Playlist',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
